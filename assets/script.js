e0304c7888da72937f50e1760ee52f156984a3cd
SoloJette <solemansay@gmail.com>
2020-08-10 22:28:31 -0400

    added the js for reactive coloring

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,14 @@
 
 $("#currentDay").text(moment().format("dddd MMMM Do YYYY h:mm a"));
 
+$(".row").each(function () {
+    if ($(this)[0].children[1].id == ct) {
+        $(this).find(".ta").addClass("present")
+    }
+    else if ($(this)[0].children[1].id < ct) {
+        $(this).find(".ta").addClass("past")
+    }
+    else{
+        $(this).find(".ta").addClass("future")
+    }
+})
