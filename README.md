47d852c8a01cdf25b495b82f3e6134451aa8a63c
SoloJette <solemansay@gmail.com>
2020-08-10 23:16:02 -0400

    adding to the readme, also made the js code more concise

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Day-Planner
+
+The reason for making this day planner was to create a website that would save and store your information for the day in the provided text areas. while doing this excersise I was able to flex the new knowledge that I have on the DOM as well as creating loops. 
+
+# how it works 
+
+The first thing that I accomplished was establishing the html, assigning it rows and columns for the time, text area and save button like so:
+
+<img src = "assets/htmlRows.jpg" alt= "html Rows and Columns example">
+
+I gave each text area an id corilating to the time of the row it was on in military time. 
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,14 @@
  var ct = moment().format("H")
  
+ //date and time 
  $("#currentDay").text(moment().format("dddd MMMM Do YYYY h:mm a"));
 
+ // On load function that will simultaniously set the color for past preasent future and getting the 
+ //values from local storage and setting them as the text area value.
+$( document ).ready(function() {
 $(".row").each(function () {
     if ($(this)[0].children[1].id == ct) {
-        $(this).find(".ta").addClass("present")
+    $(this).find(".ta").addClass("present")
     }
     else if ($(this)[0].children[1].id < ct) {
         $(this).find(".ta").addClass("past")
@@ -12,18 +16,15 @@ $(".row").each(function () {
     else{
         $(this).find(".ta").addClass("future")
     }
+        var keyName = $(this)[0].children[1].id
+        var item = localStorage.getItem(keyName)
+        $(this)[0].children[1].children[0].value = item; 
+    })
 })
 
+//Saving the value of the text area in local storage 
 $("button").on("click", function(){
     var txtValue = $(this)[0].previousElementSibling.children[0].value
     var id = $(this)[0].previousElementSibling.id
     localStorage.setItem(id, txtValue)
-})
-
-$( document ).ready(function() {
-    $(".row").each(function () {
-        var keyName = $(this)[0].children[1].id
-        var item = localStorage.getItem(keyName)
-        $(this)[0].children[1].children[0].value = item;
-    })
 })
